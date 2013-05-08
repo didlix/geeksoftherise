cdba32570fce4cabe215c0e0a34840537ea23837
Rachel Graves <rachel@didlix.co.uk>
2013-05-08 20:09:26 +0100

    Adds Padrino-Warden gem

--- project/app/app.rb
+++ project/app/app.rb
@@ -7,6 +7,38 @@ module Project
 
     enable :sessions
 
+
+    register Padrino::Warden
+
+      class User
+        attr_reader :name
+        def initialize(name)
+          @name=name
+        end
+
+        def self.authenticate(a, b)
+          return User.new('john')
+        end
+      end
+
+      Warden::Strategies.add(:password) do
+        def valid?
+          params["email"] || params["password"]
+        end
+
+        def authenticate!
+          u = User.authenticate(params["email"], params["password"])
+          u.nil? ? fail!("Could not log in") : success!(u)
+        end
+      end
+
+      Warden::Manager.serialize_into_session do |user|
+        user.id
+      end
+
+      Warden::Manager.serialize_from_session do |id|
+        User.get(id)
+      end
     ##
     # Caching support
     #
